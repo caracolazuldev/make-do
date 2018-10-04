4c0179fafed1d4ece73891ec05e3ee7fe4ad9f29
ginkgoMZD <mzd@ginkgostreet.com>
2018-10-04 17:15:10 -0400

    whitespace

--- util/cli.mk
+++ util/cli.mk
@@ -1,12 +1,12 @@
 
 define user-confirm
 	@while true; do \
-	  read -p ${1}'[y/n]: ' yn ;\
-	  case $$yn in \
-      [Yy]* ) break;; \
-      [Nn]* ) echo "Aborted" && exit 1;; \
-      * ) echo "Please answer yes or no.";; \
-	  esac \
+	  read -p $(strip ${1})' [y/n]: ' yn ;\
+		case $$yn in \
+			[Yy]* ) break;; \
+			[Nn]* ) echo "Aborted" && exit 1;; \
+			* ) echo "Please answer yes or no.";; \
+		esac \
 	done \
 
 endef
