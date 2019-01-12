52974b7ca9c3914bd63960eecf2f5a48bdb60182
ginkgoMZD <mzd@ginkgostreet.com>
2019-01-12 11:42:34 -0500

    avoid erronious error; don't suppress cmd echo;

--- util/install-cmd.mk
+++ util/install-cmd.mk
@@ -4,5 +4,6 @@ BIN_DIR ?= /usr/local/bin
 util-install-cmd: CMD_DIR ?= $(THIS_DIR)
 util-install-cmd: CMD_FILE ?= $(notdir $(CMD_DIR))
 util-install-cmd:
-	@ test -L $(BIN_DIR)/$(CMD_FILE) || sudo ln -s $(CMD_DIR)/$(CMD_FILE) $(BIN_DIR)/$(CMD_FILE)
-	@ test -f .completions && sudo cp .completions /etc/bash_completion.d/$(CMD_FILE).sh
+	test -L $(BIN_DIR)/$(CMD_FILE) || sudo -n ln -s $(CMD_DIR)/$(CMD_FILE) $(BIN_DIR)/$(CMD_FILE)
+	@# always use an || with test so it is not interpreted as an error by make:
+	test -f .completions && sudo cp .completions /etc/bash_completion.d/$(CMD_FILE).sh || true
