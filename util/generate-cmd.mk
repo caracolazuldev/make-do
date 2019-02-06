8ed626dc6ea8f41a11851f080759ada5ff1ea323
ginkgoMZD <mzd@ginkgostreet.com>
2019-02-06 00:27:12 -0500

    normalize braces

--- util/generate-cmd.mk
+++ util/generate-cmd.mk
@@ -1,12 +1,12 @@
 
-util-generate-cmd: CMD_DIR ?= $(THIS_DIR)
-util-generate-cmd: CMD_FILE ?= $(notdir $(CMD_DIR))
+util-generate-cmd: CMD_DIR ?= ${THIS_DIR}
+util-generate-cmd: CMD_FILE ?= $(notdir ${CMD_DIR}))
 util-generate-cmd:
-	@ test ! -f "$(CMD_FILE)" || ( echo command would be overwritten && false )
-	@ echo '#!/usr/bin/env bash' > $(CMD_FILE)
-	@ echo >> $(CMD_FILE)
-	echo make -C $(CMD_DIR) '"$$@"' >> $(CMD_FILE)
-	chmod ug+x $(CMD_FILE)
+	@ test ! -f "${CMD_FILE}" || ( echo command would be overwritten && false )
+	@ echo '#!/usr/bin/env bash' > ${CMD_FILE}
+	@ echo >> ${CMD_FILE}
+	echo make -C ${CMD_DIR} '"$$@"' >> ${CMD_FILE}
+	chmod ug+x ${CMD_FILE}
 
 # TODO
 # util-generate-completions:
