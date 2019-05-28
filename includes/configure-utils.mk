6131d477a1a6c6481302ab7012765ca6012a03ec
ginkgoMZD <mzd@ginkgostreet.com>
2019-05-27 20:27:13 -0400

    fix bugs; allow absence of comments in config templates; more documentation;

--- includes/configure-utils.mk
+++ includes/configure-utils.mk
@@ -10,13 +10,19 @@
 #
 # WARNING: escape spaces in paths your config-include list. 
 #
+# WARNING: ignores lines that do not contain an 'export'.
+# SAMPLE: export PROJECT_ROOT ?= /var/www/# comments like this are used in user-prompts.
+#	prompt: export PROJECT_ROOT ( comments liek this are used in user-prompts. ) = [/var/www/]?
+#
+# TIP: include a trailing-slash when configuring paths. 
+# TIP: terminate paths with a hash/sharp to avoid the error of trailing-white-space.
+#
 # FEATURES:
 # Auto-includes the files in the CONFIG_INCLUDES list.
 #
 # Provides a target, "configure" that will interactively prompt you to enter the config values. Since the config file is always loaded first, you can run configure multiple times, and existing configs will be loaded as default values.
+#
 # # #
-
-include ${CONFIG_INCLUDES}
 
 # # #
 # Shell-escape spaces.
@@ -27,14 +33,10 @@ space += # hack that exploits implicit space added when concatenating assignment
 escape-spaces = $(subst ${space},\${space},$(strip $1))
 
 # # #
-# Shell command to replace {{TOKENs}} in a file
-# Copy a template and then replace the tokens in the copy.
-#
-REPLACE_TOKENS = perl -p -i -e 's%\{\{([^}]+)\}\}%defined $$ENV{$$1} ? $$ENV{$$1} : $$&%eg'
-
-# # #
 # keep track of this location
 this-dir := $(call escape-spaces,$(realpath $(dir $(lastword $(MAKEFILE_LIST)))))/
+
+include ${CONFIG_INCLUDES}
 
 %.conf: | ${this-dir}prompt-for-configs.sh-is-exec
 	${this-dir}prompt-for-configs.sh ${*}.tpl ${@}
@@ -45,4 +47,8 @@ configure:
 %-is-exec:
 	chmod a+x ${*}
 
-.DEFAULT_GOAL := 
+# # #
+# Shell command to replace {{TOKENs}} in a file
+# Copy a template and then replace the tokens in the copy.
+#
+REPLACE_TOKENS = perl -p -i -e 's%\{\{([^}]+)\}\}%defined $$ENV{$$1} ? $$ENV{$$1} : $$&%eg'
--- includes/prompt-for-configs.sh
+++ includes/prompt-for-configs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # # #
-# Generates an include file for Gnu Make from scoped environment variables.
+# Generates an include file for Gnu Make using scoped environment variables to replace tokens in a template.
 # 
 # Example implicit rule for generating .conf files from .tpl files:
 # %.conf: 
@@ -10,22 +10,34 @@
 # Values can only come from the environment, or from user-prompt.
 # To re-configure a file, including the config file will cause the existing values to be used by default.
 #
+# WARNING: ignores lines that do not contain an 'export'.
+# SAMPLE: export PROJECT_ROOT ?= /var/www/# comments like this are used in user-prompts.
+#	prompt: export PROJECT_ROOT ( comments liek this are used in user-prompts. ) = [/var/www/]?
+#
+# TIP: include a trailing-slash when configuring paths. 
+# TIP: terminate paths with a hash/sharp to avoid the error of trailing-white-space.
+#
 # Example re-configure target where ${CONFIG_INCLUDES} contains a list of .conf files:
 # configure:
 #	$(MAKE) -RrB ${CONFIG_INCLUDES} 	# -Rr, doesn't load special vars or targets; -B forces re-building (the config files)
 #
 # # #
 
-
 printf '\nGenerating Configuration.\n'
 printf '\nLeave blank for default value.\n'
 
 # get vars to set from the tpl:
 while IFS= read -r conf; do
-	test "${conf#*export}" = "$conf" && continue;
+	test "${conf#*export}" = "$conf" && continue;	# discard lines that don't begin with 'export'
+
 	the_var=$(echo "$conf" | sed -s 's/export[[:space:]]*\([^[:space:]]*\).*/\1/')
-	help=$(echo "$conf" | sed -s 's/[^#]*#\(.*\)$/(\1 ) / ')
+	help=$(echo "$conf" | sed -s 's/[^#]*#*\([^#]*\)$/\1/')
+	test -n "$help" && help="($help ) "
 	default=$(eval echo -n \"'$'$the_var\")
+	# dev detritus:
+	#echo found the_var="$the_var"
+	#echo found help="$help"
+	#echo found default="$default"
 	printf 'export %s %s= [%s]? ' "$the_var" "$help" "$default"
 	read reply </dev/tty
 	if [ -z "$reply" ]; then
@@ -39,9 +51,10 @@ printf '\nReview Changes:\n'
 while IFS= read -r conf; do
 	test "${conf#*export}" = "$conf" && continue;
 	the_var=$(echo "$conf" | sed -s 's/export[[:space:]]*\([^[:space:]]*\).*/\1/')
-	help=$(echo "$conf" | sed -s 's/[^#]*#\(.*\)$/\1/ ')
+	help=$(echo "$conf" | sed -s 's/[^#]*#*\([^#]*\)$/\1/')
+	test -n "$help" && help="#($help ) "
 	default=$(eval echo -n \"'$'$the_var\")
-	printf 'export %s = %s#%s\n' "$the_var" "$default" "$help" 
+	printf 'export %s = %s%s\n' "$the_var" "$default" "$help" 
 done < "$1"
 
 printf '\nCommit these changes? [Y/n]'
