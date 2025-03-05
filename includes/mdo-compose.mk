e9e81b7a3247922640d4ab21692c9feb4b18bd7f
Michael Z Daryabeygi <michael@caracolazul.dev>
2025-03-05 11:46:38 -0500

    allow override of list of wrapped commands. rm 'build' as it could easily conflict.

--- includes/mdo-compose.mk
+++ includes/mdo-compose.mk
@@ -51,17 +51,20 @@ exec-or-run = $(if $(call task-up,$1),exec,run --rm) $2 $1
 # Commands we especially want to ensure have an up-to-date compose.yaml file
 # or that we alter the default behavior (e.g. up -d, run --rm)
 # and a couple included for symmetry (down, exec)
-SUB_CMD_ABLE := pull build up down run exec logs
+# If your command arguments conflict with this list, you will have to
+# override it to exclude the keyword that appears in your command, and 
+# this feature will not be available for that docker-compose command.
+SUB_CMD_ABLE ?= up down run exec logs
 
 # passes additional arguments to docker compose using $(filter-out $@,$(MAKECMDGOALS))
-${SUB_CMD_ABLE} : .env ${COMPOSE_FILENAME}
+${SUB_CMD_ABLE}: .env ${COMPOSE_FILENAME}
 	$(DKC) $@ \
 		$(if $(filter $@,up),-d) \
 		$(if $(filter $@,run),--rm) \
 		$(if $(filter $@,logs),--follow) \
 		$(filter-out $@,$(MAKECMDGOALS))
 
-ifneq ($(filter ${SUB_CMD_ABLE},$(MAKECMDGOALS)),)
+ifneq (,$(filter ${SUB_CMD_ABLE},$(MAKECMDGOALS)))
 # Prevent errors when non-targets are passed for sub-commands
 %:
 	@# No-Op
@@ -77,27 +80,29 @@ endif
 # Does not descend into subdirectories (-maxdepth 1)
 define default-env-includes
 $(strip \
-	$(shell find conf -maxdepth 1 -type f -name '*.env' | sort) \
+	$(shell find conf config configs -maxdepth 1 -type f -name '*.env' 2>/dev/null | sort) \
 )
 endef
 
 ENV_INCLUDES ?= ${default-env-includes}
 
 .env: ${ENV_INCLUDES}
-ifndef ENV_INCLUDES
-	$(error ENV_INCLUDES is not set)
-endif
+	@ echo '# ' > $@
+	@ echo '# WARNING: Generated Configuration using - $^' >> $@
+	@ echo '# ' >> $@
+ifeq (,${ENV_INCLUDES})
+	$(warning WARN: ENV_INCLUDES is not set)
+else
 	@# Ensure that each file ends with a newline
 	@for file in $^; do \
 		if [ -n "$$(tail -c 1 "$$file" | tr -d '\n')" ]; then \
 			echo >> "$$file"; \
 		fi; \
 	done
-	@ echo '# ' > $@
-	@ echo '# WARNING: Generated Configuration using - $^' >> $@
-	@ echo '# ' >> $@
 	@cat $^ >>$@
 	@echo "Generated .env using $^"
+endif
+
 
 #
 # Generate the Compose File
@@ -110,8 +115,9 @@ endif
 # Does not descend into subdirectories (-maxdepth 1)
 define default-compose-file-includes
 $(strip \
-	$(shell find docker -maxdepth 1 -type f \
+	$(shell find docker containers -maxdepth 1 -type f \
 		-name '*.yml' -o -name '*.yaml' \
+		2>/dev/null \
 		| grep -v profile | sort) \
 	$(shell find docker -maxdepth 1 -type f \
 		-name '*.profile.yml' -o -name '*.profile.yaml' | sort) \
@@ -125,7 +131,7 @@ $(info COMPOSE_FILES=${COMPOSE_FILES})
 endif
 
 ${COMPOSE_FILENAME}: .env ${COMPOSE_FILES}
-ifndef COMPOSE_FILES
+ifeq (,COMPOSE_FILES)
 	$(error COMPOSE_FILES is not set)
 endif
 	@ echo '# ' > $@
